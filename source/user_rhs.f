3018bf7d9edf5edca8ad2521ea6c698e7c658420
Christopher Howland <cjh225@conjury.damtp.cam.ac.uk>
2018-01-31 15:03:14 +0000

    Reordered random number generation

--- source/user_rhs.f
+++ source/user_rhs.f
@@ -13,8 +13,7 @@
 
 ! **** LOW WAVENUMBER (FURUE, 2003) FORCING ****
 
-      call RANDOM_NUMBER(alpha)
-      alpha=2.*pi*alpha ! Random phase of forcing
+
       K0=7.
       F_0=1.0e+4           ! Amplitude of forcing
 
@@ -23,6 +22,8 @@
           do I=0,NKX_S
             IF ( (KX2_S(I)+KZ2_S(K)+KY2(J).LE.100.) .AND.
      &            (KY(J).NE.0) .AND. (KX2_S(I)+KZ2_S(K).NE.0) ) THEN
+              call RANDOM_NUMBER(alpha)
+              alpha=2.*pi*alpha ! Random phase of forcing
               CS1(I,K,J)=F_0*cexp(cmplx(0,alpha))*(8.*pi)**(-0.5)*
      &                      (KX2_S(I)+KZ2_S(K)+KY2(J)+K0**2)**(-3)
               CF1(I,K,J)=CF1(I,K,J)+CS1(I,K,J)*KY(J)*KX_S(I)/
@@ -31,24 +32,14 @@
      &       (KX2_S(I)+KZ2_S(K)+KY2(J))**(1./4.)/SQRT(KX2_S(I)+KZ2_S(K))
               CF3(I,K,J)=CF3(I,K,J)+CS1(I,K,J)*SQRT(KX2_S(I)+KZ2_S(K))/
      &           (KX2_S(I)+KZ2_S(K)+KY2(J))**(1./4.)
-            END IF
-          end do
-        END DO
-      END DO
-
-      DO J=0,TNKY_TH
-        DO K=0,TNKZ_S_TH
-          do I=0,NKX_S_TH
-          IF ((KX2_S_TH(I)+KZ2_S_TH(K)+KY2_TH(J).LE.100.).AND.
-     &        (KY_TH(J).NE.0).AND.(KX2_S_TH(I)+KZ2_S_TH(K).NE.0)) THEN
-              CSTH1(I,K,J)=cexp(cmplx(0,alpha))*(8.*pi)**(-0.5)*
-     &                  (KX2_S_TH(I)+KZ2_S_TH(K)+KY2_TH(J)+K0**2)**(-3)
-              CFTH(I,K,J,1)=CFTH(I,K,J,1)+CSTH1(I,K,J)*CI*
+              CFTH(I,K,J,1)=CFTH(I,K,J,1)+CS1(I,K,J)*CI*
      &       (KX2_S_TH(I)+KZ2_S_TH(K)+KY2_TH(J))**(1./4.)
             END IF
           end do
         END DO
       END DO
+
+      
 
 ! For example, to add a linear damping term (e.g. -alpha*U) to the RHS:
 !       alpha=-0.1d0
