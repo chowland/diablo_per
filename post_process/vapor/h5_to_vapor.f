f3bf5073f0a4f36f472fca081ca859a03f9ffbe3
Christopher Howland <cjh225@conjury.damtp.cam.ac.uk>
2018-02-01 17:23:06 +0000

    Added Vapor post-processing files

---
+++ post_process/vapor/h5_to_vapor.f
@@ -0,0 +1,381 @@
+      program res_to_vapor
+! This program converts a diablo restart file to a format readable by vapor
+! A specified number of wavelet transforms are performed to produce
+! compressed versions of the data
+! Note, as of version 1.1.2, Vapor cannot handle stretched grids
+
+! grid_def should contain the grid size and number of scalars
+      include 'header'
+
+      character*85 FNAME
+
+! Define Variables
+      integer i,j,k,n
+      real buffer3(0:NX-1,0:NZ-1,0:NY-1)
+! The sizes of these strings may need to be changed
+      character*15 size_str
+      character*55 len_str,run_dir
+      character*4 NUM_TIMES
+
+! **** User Input *****
+! Variables for movie
+      integer FIRST_OUT, LAST_OUT, OUT_NUM, loop_index
+      FIRST_OUT=1
+      LAST_OUT=16
+      NUM_TIMES='17' ! Remember to add 1 for start.h5 here
+
+! Number of periodic directions used in the simulation
+      NUM_PER_DIR=3
+! This string should contain the size of the buffer array
+      size_str='128x128x128'
+      len_str='0.0:0.0:0.0:6.28:6.28:6.28'
+      LX=6.28
+      LY=6.28
+      LZ=6.28
+
+      run_dir='../../../scratch_backup/create_th_test' ! Run directory
+
+!      RI_TAU(1)=1.0d0
+
+      NXM=NX-1
+      NYM=NY-1
+      NZM=NZ-1
+
+      call init_fft
+
+         DO I=0,NXM
+           GX(I)=(DBLE(I)*LX)/DBLE(NX)
+         END DO
+         DO J=0,NYM
+           GY(J)=(DBLE(J)*LY)/DBLE(NY)
+         END DO
+         DO K=0,NZM
+           GZ(K)=(DBLE(K)*LZ)/DBLE(NZ)
+         END DO
+
+      write(*,*) 'creating header...'
+
+! Now we are ready to create a vdf (header) file
+      if (N_TH.eq.0) then
+        call SYSTEM('vdfcreate -dimension '
+     &//size_str//' -extents '//len_str//' -periodic 1:1:1
+     &    -numts '//NUM_TIMES//' -level 3 -vars3d U1:U2:U3
+     &     vapor.vdf')
+      else if (N_TH.eq.1) then
+        call SYSTEM('vdfcreate -dimension '
+     &//size_str//' -extents '//len_str//' -periodic 1:1:1
+     &    -numts '//NUM_TIMES//' -level 3 -vars3d U1:U2:U3:TH1
+     &      vapor.vdf')
+      else if (N_TH.eq.2) then
+        call SYSTEM('vdfcreate -dimension '
+     &//size_str//' -extents '//len_str//' -periodic 1:1:1
+     &    -numts '//NUM_TIMES//' -level 3 -vars3d U1:U2:U3:TH1:TH2
+     &      vapor.vdf')
+      else if (N_TH.eq.3) then
+        call SYSTEM('vdfcreate -dimension '
+     &//size_str//' -extents '//len_str//' -periodic 1:1:1
+     &    -numts '//NUM_TIMES//' -level 3 -vars3d U1:U2:U3:TH1:TH2:TH3
+     &      vapor.vdf')
+      end if
+
+      FNAME=trim(run_dir)//'/start.h5'
+      write(*,*) 'reading flow from ',FNAME
+      call ReadHDF5(FNAME)
+
+      do i=0,NXM
+        do j=0,NYM
+          do k=0,NZM
+            buffer3(i,k,j)=real(U1(i,k,j))
+          end do
+        end do
+      end do
+      open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+      write(22,REC=1) buffer3
+      close(22)
+      write(*,*) 'raw2vdf for U1...'
+      call SYSTEM('raw2vdf -ts 0 -varname U1 vapor.vdf temp.raw')
+
+      do i=0,NXM
+        do j=0,NYM
+          do k=0,NZM
+            buffer3(i,k,j)=real(U2(i,k,j))
+          end do
+        end do
+      end do
+      open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+      write(22,REC=1) buffer3
+      close(22)
+      write(*,*) 'raw2vdf for U2...'
+      call SYSTEM('raw2vdf -ts 0 -varname U2 vapor.vdf temp.raw')
+
+      do i=0,NXM
+        do j=0,NYM
+          do k=0,NZM
+            buffer3(i,k,j)=real(U3(i,k,j))
+          end do
+        end do
+      end do
+      open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+      write(22,REC=1) buffer3
+      close(22)
+      write(*,*) 'raw2vdf for U3...'
+      call SYSTEM('raw2vdf -ts 0 -varname U3 vapor.vdf temp.raw')
+
+      if (N_TH.ge.1) then
+        do i=0,NXM
+          do j=0,NYM
+            do k=0,NZM
+              buffer3(i,k,j)=real(TH(i,k,j,1))
+            end do
+          end do
+        end do
+        open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+        write(22,REC=1) buffer3
+        close(22)
+        write(*,*) 'raw2vdf for TH1...'
+        call SYSTEM('raw2vdf -ts 0 -varname TH1 vapor.vdf temp.raw')
+      end if
+
+      if (N_TH.ge.2) then
+        do i=0,NXM
+          do j=0,NYM
+            do k=0,NZM
+              buffer3(i,k,j)=real(TH(i,k,j,2))
+            end do
+          end do
+        end do
+        open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+        write(22,REC=1) buffer3
+        close(22)
+        write(*,*) 'raw2vdf for TH2...'
+        call SYSTEM('raw2vdf -ts 0 -varname TH2 vapor.vdf temp.raw')
+      end if
+
+      if (N_TH.ge.3) then
+        do i=0,NXM
+          do j=0,NYM
+            do k=0,NZM
+              buffer3(i,k,j)=real(TH(i,k,j,3))
+            end do
+          end do
+        end do
+        open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+        write(22,REC=1) buffer3
+        close(22)
+        write(*,*) 'raw2vdf for TH3...'
+        call SYSTEM('raw2vdf -ts 0 -varname TH3 vapor.vdf temp.raw')
+      end if
+
+      DO OUT_NUM=FIRST_OUT,LAST_OUT
+        loop_index=OUT_NUM-FIRST_OUT+1
+        if (LAST_OUT.lt.10) then
+          FNAME=trim(run_dir)//'/restart_files/out'
+     &        //char(mod(OUT_NUM,10)+48)//'.h5'
+        else if (LAST_OUT.lt.100) then
+          FNAME=trim(run_dir)//'/restart_files/out'
+     &        //char(mod(OUT_NUM,100)/10+48)
+     &        //char(mod(OUT_NUM,10)+48)//'.h5'
+        else if (LAST_OUT.lt.1000) then
+          FNAME=trim(run_dir)//'/restart_files/out'
+     &        //char(mod(OUT_NUM,1000)/100+48)
+     &        //char(mod(OUT_NUM,100)/10+48)
+     &        //char(mod(OUT_NUM,10)+48)//'.h5'
+        else
+          stop 'Error: too many output files'
+        end if
+        write(*,*) 'reading flow from ',FNAME
+        call ReadHDF5(FNAME)
+
+        do i=0,NXM
+          do j=0,NYM
+            do k=0,NZM
+              buffer3(i,k,j)=real(U1(i,k,j))
+            end do
+          end do
+        end do
+        open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+        write(22,REC=1) buffer3
+        close(22)
+        write(*,*) 'raw2vdf for U1...'
+        if (loop_index.lt.10) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U1 vapor.vdf temp.raw')
+        else if (loop_index.lt.100) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U1 vapor.vdf temp.raw')
+        else if (loop_index.lt.1000) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,1000)/100+48)
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U1 vapor.vdf temp.raw')
+        end if
+
+        do i=0,NXM
+          do j=0,NYM
+            do k=0,NZM
+              buffer3(i,k,j)=real(U2(i,k,j))
+            end do
+          end do
+        end do
+        open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+        write(22,REC=1) buffer3
+        close(22)
+        write(*,*) 'raw2vdf for U2...'
+        if (loop_index.lt.10) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U2 vapor.vdf temp.raw')
+        else if (loop_index.lt.100) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U2 vapor.vdf temp.raw')
+        else if (loop_index.lt.1000) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,1000)/100+48)
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U2 vapor.vdf temp.raw')
+        end if
+
+        do i=0,NXM
+          do j=0,NYM
+            do k=0,NZM
+              buffer3(i,k,j)=real(U3(i,k,j))
+            end do
+          end do
+        end do
+        open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+        write(22,REC=1) buffer3
+        close(22)
+        write(*,*) 'raw2vdf for U3...'
+        if (loop_index.lt.10) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U3 vapor.vdf temp.raw')
+        else if (loop_index.lt.100) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U3 vapor.vdf temp.raw')
+        else if (loop_index.lt.1000) then
+          call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,1000)/100+48)
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname U3 vapor.vdf temp.raw')
+        end if
+
+        if (N_TH.ge.1) then
+          do i=0,NXM
+            do j=0,NYM
+              do k=0,NZM
+                buffer3(i,k,j)=real(TH(i,k,j,1))
+              end do
+            end do
+          end do
+          open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+          write(22,REC=1) buffer3
+          close(22)
+          write(*,*) 'raw2vdf for TH1...'
+          if (loop_index.lt.10) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH1 vapor.vdf temp.raw')
+          else if (loop_index.lt.100) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH1 vapor.vdf temp.raw')
+          else if (loop_index.lt.1000) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,1000)/100+48)
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH1 vapor.vdf temp.raw')
+          end if
+        end if
+
+        if (N_TH.ge.2) then
+          do i=0,NXM
+            do j=0,NYM
+              do k=0,NZM
+                buffer3(i,k,j)=real(TH(i,k,j,2))
+              end do
+            end do
+          end do
+          open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+          write(22,REC=1) buffer3
+          close(22)
+          write(*,*) 'raw2vdf for TH2...'
+          if (loop_index.lt.10) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH2 vapor.vdf temp.raw')
+          else if (loop_index.lt.100) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH2 vapor.vdf temp.raw')
+          else if (loop_index.lt.1000) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,1000)/100+48)
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH2 vapor.vdf temp.raw')
+          end if
+        end if
+
+        if (N_TH.ge.3) then
+          do i=0,NXM
+            do j=0,NYM
+              do k=0,NZM
+                buffer3(i,k,j)=real(TH(i,k,j,3))
+              end do
+            end do
+          end do
+          open (22,file='temp.raw',form='UNFORMATTED',
+     &    ACCESS='DIRECT',RECL=NX*NY*NZ)
+          write(22,REC=1) buffer3
+          close(22)
+          write(*,*) 'raw2vdf for TH3...'
+          if (loop_index.lt.10) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH3 vapor.vdf temp.raw')
+          else if (loop_index.lt.100) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH3 vapor.vdf temp.raw')
+          else if (loop_index.lt.1000) then
+            call SYSTEM('raw2vdf -ts '
+     &      //char(mod(loop_index,1000)/100+48)
+     &      //char(mod(loop_index,100)/10+48)
+     &      //char(mod(loop_index,10)+48)
+     &      //' -varname TH3 vapor.vdf temp.raw')
+          end if
+        end if
+
+      end do
+
+!      call SYSTEM('rm -f temp.raw')
+
+!     We are done
+      stop
+      end
---
+++ post_process/vapor/read_h5s.f
@@ -0,0 +1,242 @@
+C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
+      SUBROUTINE ReadHDF5(FNAME)
+C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
+      use hdf5
+
+      INCLUDE 'header'
+
+      CHARACTER*85 FNAME
+      LOGICAL FINAL, READ_PRESSURE
+
+      REAL*8 tmp(NX,NY,NZ)
+      REAL*8 tmp_th(NX_TH,NY_TH,NZ_TH)
+
+c     Dataset names
+      character(len=10) :: dname
+
+c     Identifiers
+      integer(hid_t) :: file_id, dset_id
+      integer(hid_t) :: filspace_id, memspace_id
+      integer(hid_t) :: filspace_id_th, memspace_id_th
+
+!     Identifiers
+      integer(hid_t) :: selspace_id
+      integer(hid_t) :: plist_id_w,plist_id_d
+
+c     Dimensions in the memory and in the file
+      integer(hsize_t), dimension(3) :: dimsm
+      integer(hsize_t), dimension(3) :: dimsm_th
+
+      integer(hsize_t), dimension(3) :: count, offset_f, offset_m
+      integer(hsize_t), dimension(3) :: stride, block
+      integer(hsize_t), dimension(3) :: offset_th, block_th
+
+      integer :: rHDF5 = 3, i,j,k
+
+      integer(hsize_t),dimension(2) :: adims
+      integer(hid_t)                :: aid
+      integer, dimension(2)         :: tint(3,2)
+      character*80                  :: namnbuf
+      character*20                  :: sttimec
+
+      integer error, ith
+
+      double precision En(4)
+
+      dimsm(1:3) = (/NX,NY,NZ/)
+      block = dimsm
+      dimsm_th(1:3) = (/NX_TH,NY_TH,NZ_TH/)
+      block_th = dimsm_th
+
+!     Stride and count for number of rows and columns in each dimension
+      stride = 1
+      count  = 1
+
+!     Offset determined by the rank of a processor
+      offset_f(1:3) = 0
+      offset_th(1:3) = 0
+      offset_m(1:3) = 0
+
+!     Initialize interface
+      call h5open_f(error)
+
+!     Open the file
+      call h5fopen_f(trim(FNAME), H5F_ACC_RDONLY_F,
+     &                 file_id, error)
+
+!     -----------------------------
+!     Resolution
+!     -----------------------------
+      adims=(/3,2/)
+      call h5aopen_by_name_f(file_id,'.','Resolution',aid,error)
+      call h5aread_f(aid,H5T_NATIVE_INTEGER,tint,adims,error)
+      call h5aclose_f(aid, error)
+      ! Check that the resolution is of the same kind
+      if ((tint(1,1).ne. NX)  .or.  (tint(2,1).ne.NY)  .or.
+     &    (tint(3,1).ne.NZ) .or. (tint(1,2).ne.NX_TH) .or.
+     &    (tint(2,2).ne.NY_TH) .or. (tint(3,2).ne.NZ_TH)) then
+        write(*,*) ' Error. File and program have ',
+     &        'different resolutions. '
+        write(*,*) ' Program: ', NX,NY,NZ,NX_TH,NY_TH,NZ_TH
+        write(*,*) ' File   : ', tint(1:3,1),tint(1:3,2)
+        stop
+      end if
+
+!     -----------------------------
+!     Time stamps
+!     -----------------------------
+      adims=(/1,80/)
+      call h5aopen_by_name_f(file_id,'.','Time',aid,error)
+      call h5aread_f(aid,H5T_NATIVE_DOUBLE,TIME,adims,error)
+      call h5aclose_f(aid, error)
+
+      call h5aopen_by_name_f(file_id,'.','Timestep',aid,error)
+      call h5aread_f(aid,H5T_NATIVE_INTEGER,TIME_STEP,adims,error)
+      call h5aclose_f(aid, error)
+!     -----------------------------
+
+!     Dataspaces in memory
+      call h5screate_simple_f(rHDF5, dimsm, memspace_id, error)
+      call h5screate_simple_f(rHDF5, dimsm_th, memspace_id_th, error)
+
+      do ith=1,3+N_TH
+        write(*,*) 'ith: ',ith
+!     Here it starts the loop--->
+        select case(ith)
+        case (1)
+          dname="U"
+        case (2)
+          dname="V"
+        case (3)
+          dname="W"
+        case (4:)
+          dname="TH"//CHAR(ith+45)
+        end select
+
+        if (ith.le.3) then
+          call h5dopen_f(file_id,trim(dname),dset_id,error)
+          call h5dget_space_f(dset_id,filspace_id,error)
+! Select hyperslab in the file.
+          call h5sselect_hyperslab_f (filspace_id, H5S_SELECT_SET_F,
+     &        offset_f, count, error, stride, block)
+          call h5sselect_hyperslab_f (memspace_id, H5S_SELECT_SET_F,
+     +        offset_m, count, error, stride, block)
+! Read the dataset collectively
+          call h5dread_f(dset_id, H5T_NATIVE_DOUBLE, tmp,
+     +        dimsm, error, file_space_id = filspace_id,
+     +        mem_space_id = memspace_id)
+          select case(ith)
+          case (1)
+          do j=0,NY-1
+            do k=0,NZ-1
+              do i=0,NX-1
+                U1(i,k,j)=tmp(i+1,j+1,k+1)
+                end do
+            end do
+          end do
+          case (2)
+          do j=0,NY-1
+            do k=0,NZ-1
+              do i=0,NX-1
+                U2(i,k,j)=tmp(i+1,j+1,k+1)
+                end do
+            end do
+          end do
+          case (3)
+          do j=0,NY-1
+            do k=0,NZ-1
+              do i=0,NX-1
+                U3(i,k,j)=tmp(i+1,j+1,k+1)
+                end do
+            end do
+          end do
+          end select
+
+! Close dataset
+          call h5sclose_f(filspace_id, error)
+          call h5dclose_f(dset_id, error)
+
+        else
+! Check to make sure that we should read in this scalar
+          call h5dopen_f(file_id,trim(dname),dset_id,error)
+          call h5dget_space_f(dset_id,filspace_id_th,error)
+! Select hyperslab in the file.
+          call h5sselect_hyperslab_f (filspace_id_th, H5S_SELECT_SET_F,
+     &        offset_th, count, error, stride, block)
+          call h5sselect_hyperslab_f (memspace_id_th, H5S_SELECT_SET_F,
+     +        offset_m, count, error, stride, block)
+! Read the dataset collectively
+          call h5dread_f(dset_id, H5T_NATIVE_DOUBLE, tmp_th,
+     +        dimsm_th, error, file_space_id = filspace_id_th,
+     +        mem_space_id = memspace_id_th)
+          do j=0,NY_TH-1
+            do k=0,NZ_TH-1
+              do i=0,NX_TH-1
+                TH(i,k,j,ith-3)=tmp_th(i+1,j+1,k+1)
+              end do
+            end do
+          end do
+
+! Close dataset
+          call h5sclose_f(filspace_id_th, error)
+          call h5dclose_f(dset_id, error)
+        end if
+
+      end do
+
+! Decide whether to compute the pressure or to read
+      call h5lexists_f(file_id, 'P', READ_PRESSURE, error)
+      if (READ_PRESSURE) then
+        dname="P"
+        call h5dopen_f(file_id,trim(dname),dset_id,error)
+        call h5dget_space_f(dset_id,filspace_id,error)
+
+! Select hyperslab in the file.
+        call h5sselect_hyperslab_f (filspace_id, H5S_SELECT_SET_F,
+     &        offset_f, count, error, stride, block)
+        call h5sselect_hyperslab_f (memspace_id, H5S_SELECT_SET_F,
+     +        offset_m, count, error, stride, block)
+
+! Write the dataset collectively
+        call h5dread_f(dset_id, H5T_NATIVE_DOUBLE, tmp,
+     +        dimsm, error, file_space_id = filspace_id,
+     +        mem_space_id = memspace_id)
+
+      do j=0,NY-1
+        do k=0,NZ-1
+          do i=0,NX-1
+            P(i,k,j)=tmp(i+1,j+1,k+1)
+            end do
+        end do
+      end do
+! Close dataset
+        call h5sclose_f(filspace_id, error)
+        call h5dclose_f(dset_id, error)
+!        call fft_xzy_mpi_to_fourier(P,CP)
+      end if
+
+! Close the dataspace for the memory
+      call h5sclose_f(memspace_id, error)
+      call h5sclose_f(memspace_id_th, error)
+
+      ! Close file
+      call h5fclose_f(file_id, error)
+      call h5close_f(error)
+
+!      IF (VARIABLE_DT) THEN
+!        CALL COURANT_MPI
+!      END IF
+
+      ! Convert to Fourier space
+!      call fft_xzy_mpi_to_fourier(U1,CU1)
+!      call fft_xzy_mpi_to_fourier(U2,CU2)
+!      call fft_xzy_mpi_to_fourier(U3,CU3)
+!      do ith=1,N_TH
+!        if (.NOT.CREATE_NEW_TH(ith)) then
+!         S1(:,:,:)=TH(:,:,:,ith)
+!         call fft_xzy_mpi_to_fourier_th(S1,CS1)
+!         CTH(:,:,:,ith)=CS1(:,:,:)
+!        end if
+!      end do
+
+      end subroutine ReadHDF5
