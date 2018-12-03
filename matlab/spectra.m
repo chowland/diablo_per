54535283bdb54749b8cfce0543c91ff4d27e8e32
Christopher Howland <cjh225@tinman.damtp.cam.ac.uk>
2018-12-03 15:08:51 +0000

    Add horizontal scale decomposition of vertical spectra

--- matlab/spectra.m
+++ matlab/spectra.m
@@ -2,8 +2,12 @@
 
 Ri_t=1;           % Richardson number
 
-fname='../example_run/out01.h5';
+% fname='../example_run/out01.h5';
+% fname=['/local/scratch/public/cjh225/v2_peta4/APS/Furue_44_Ri1/' ...
+%     'restart_files/out01.h5'];
 dname={'/U1/','/U2/','/U3/','/TH1/'};
+S1=h5read(fname,'/U2/');
+Kh=1/sqrt(mean(S1(:).^2));  % Horizontal cutoff for large scale spectrum
 for n=1:4
     S1=h5read(fname,dname{n});
     [NX,NY,NZ]=size(S1);
@@ -13,9 +17,8 @@ for n=1:4
     CS1=fftn(S1/(NX*NY*NZ)); clear S1
 
     % MATLAB fft indexed (0,1,...,NX/2,-NX/2,...,-2,-1)
-    NKY=floor(NZ/3);
+    NKY=floor(NY/3);
     KY=0:NKY;
-    Kh=35;  % Horizontal cutoff for large scale spectrum
 
     CS1=0.5*CS1.*conj(CS1); % Calculate 2-sided energy spectrum
     
@@ -30,6 +33,20 @@ for n=1:4
     if n==4
         CS2=Ri_t*CS2;
     end
+    
+    KX=[0:NX/2 -(NX/2-1):-1];   KZ=[0:NZ/2 -(NZ/2-1):-1];
+    E_large{n}=zeros(1,NKY+1); E_small{n}=zeros(1,NKY+1);
+    for i=1:NX
+        for k=1:NZ
+            if KX(i)^2+KZ(k)^2<Kh^2
+                E_large{n}=E_large{n}+CS2(i,:,k);
+            else
+                E_small{n}=E_small{n}+CS2(i,:,k);
+            end
+        end
+    end
+   
+    
     E{n}=sum(sum(CS2,3),1);
     if mod(n,2)==1
         Fr{n}=KY.^2.*E{n}/Ri_t;
@@ -39,14 +56,18 @@ for n=1:4
     clear CS1 CS2
 end
 
-figure(1);
-loglog(KY,Fr{1}+Fr{3}); hold on
-loglog(KY,Fr_shear{1}+Fr_shear{3},'--');
-xlabel('$|m|$')
-ylabel('$\Phi_{Fr}$')
+Et_large=E_large{1}+E_large{2}+E_large{3};
+Et_small=E_small{1}+E_small{2}+E_small{3};
+Et=E{1}+E{2}+E{3};
 
-figure(2);
-Et=E{1}+E{2}+E{3}+E{4};
-loglog(KY,Et.*KY.^3);
-xlabel('$|m|$')
-ylabel('$|m|^3 E(|m|)$')
+% figure(1);
+% loglog(KY,Fr{1}+Fr{3}); hold on
+% loglog(KY,Fr_shear{1}+Fr_shear{3},'--');
+% xlabel('$|m|$')
+% ylabel('$\Phi_{Fr}$')
+% 
+% figure(2);
+% Et=E{1}+E{2}+E{3};%+E{4};
+% loglog(KY,Et.*KY.^3);
+% xlabel('$|m|$')
+% ylabel('$|m|^3 E(|m|)$')
